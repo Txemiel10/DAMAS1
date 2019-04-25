b6cabfb48b488549e447607464160349fc88f53e
Txemi <44409144+Txemiel10@users.noreply.github.com>
2019-04-25 15:59:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,9 +33,7 @@ this.addEventListener('fetch', event => {
   // request.mode = navigate isn't supported in all browsers
   // so include a check for Accept: text/html header.
   if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url);
-    );
+        event.respondWith(fetch(event.request.url));
   }
   else{
         // Respond with everything else if we can
